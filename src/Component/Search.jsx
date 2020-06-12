0a4aa0c5ccb0cfa18a25cb91015be165578c2a6b
artyuhvladislav <workto22@gmail.com>
2020-06-12 12:40:23 +0300

    add: add Search component

---
+++ src/Component/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Search = () => {
+    return (
+        <div className="search-container">
+            <input className="search-field" type="text" />
+            <div className="search-block">
+                <img className="search-block-icon" src="./img/search-icon.png" alt="search-icon" />
+            </div>
+
+        </div>
+    )
+}
+export default Search
