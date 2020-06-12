c518ffef27321cc8dc3653fdf0eb015c90c8a4c5
artyuhvladislav <workto22@gmail.com>
2020-06-12 12:44:34 +0300

    add: add children Components to Header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Logo from './Logo';
+import Search from './Search';
+import UserSettings from './UserSettings';
+
 const Header = () => {
     return (
         <header className="header">
-
+            <Logo />
+            <Search />
+            <UserSettings />
         </header>
     )
 }
