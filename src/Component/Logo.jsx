0932f03d6c49c4fe2f938993e10c7a822e50f11f
artyuhvladislav <workto22@gmail.com>
2020-06-12 12:39:51 +0300

    add: add Logo Component

---
+++ src/Component/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Logo = () => {
+    return (
+        <div className="logo">
+            <a href="#" className="logo-link">
+                <img src="./img/logo.png" alt="socialNetwork" className="logo-img" />
+                <h1 className="logo-title">SocialNetwork</h1>
+            </a>
+        </div>
+    )
+}
+export default Logo
