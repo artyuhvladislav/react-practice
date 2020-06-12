9fcbedb4c81966c3b11efca12c8d85397a8e7708
artyuhvladislav <workto22@gmail.com>
2020-06-12 10:28:15 +0300

    init: initialize the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Content from './Content';
+const Menu = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>first</li>
+        <li>second</li>
+        <li>third</li>
+        <li><Content /></li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default Menu;
